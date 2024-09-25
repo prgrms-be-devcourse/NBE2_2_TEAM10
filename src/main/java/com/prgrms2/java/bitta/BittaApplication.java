e1cd0333f3350428d9934d35cd90c17eb95370e7
deveunhwa <deveunhwa@gmail.com>
2024-09-25 16:20:20 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/com/prgrms2/java/bitta/BittaApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms2.java.bitta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BittaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BittaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms2/java/bitta/BittaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms2.java.bitta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BittaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
