7b5fc148f61afc5436986025e02732890fdc0781
juwon-code <juwoncode@gmail.com>
2024-09-26 15:51:09 +0900

    feat: Video ID 접근권한 변경 Video 인스턴스의 ID를 Setter로 변경할 수 없게 수정합니다.
    Related to: #14

--- src/main/java/com/prgrms2/java/bitta/entity/Video.java
+++ src/main/java/com/prgrms2/java/bitta/entity/Video.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 public class Video {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long videoId;
 
     private String title;
