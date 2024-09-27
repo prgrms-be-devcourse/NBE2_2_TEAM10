cd331e30e8f14902ffc75532af985854c6c5304a
Preta3418 <jason123qwe@gmail.com>
2024-09-27 14:30:48 +0900

    Feed Entity Photo 추가

--- src/main/java/com/prgrms2/java/bitta/feed/entity/Feed.java
+++ src/main/java/com/prgrms2/java/bitta/feed/entity/Feed.java
@@ -1,5 +1,6 @@
 package com.prgrms2.java.bitta.feed.entity;
 
+import com.prgrms2.java.bitta.photo.entity.Photo;
 import com.prgrms2.java.bitta.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
@@ -7,6 +8,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -31,4 +33,8 @@ public class Feed {
     @CreatedDate
     @Column(updatable = false, nullable = false)
     private LocalDateTime createdAt;
+
+    //사진들의 링크 집합입니다. 유저보다는 일단 feed 자체에 연결시키겠습니다
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Photo> photos;
 }
