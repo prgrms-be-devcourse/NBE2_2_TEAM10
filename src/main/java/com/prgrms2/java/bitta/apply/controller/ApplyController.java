7a56f4c3ea6a35663680c25ca46522f3e358a118
YJH <130978507+YooJHyun@users.noreply.github.com>
2024-10-01 10:19:27 +0900

    JobPost CRUD

---
+++ src/main/java/com/prgrms2/java/bitta/apply/controller/ApplyController.java
@@ -0,0 +1,11 @@
+package com.prgrms2.java.bitta.apply.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/apply")
+public class ApplyController {
+}
