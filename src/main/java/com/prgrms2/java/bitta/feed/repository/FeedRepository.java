9f45df9c85c6c7c2b1aa9b8cf97b8c834b3c7ccc
juwon-code <juwoncode@gmail.com>
2024-10-01 11:37:34 +0900

    feat: 인스턴스 제공 클래스 추가 - 각 도메인의 인스턴스를 제공하는 Provider 클래스를 추가 - 요구되는 객체를 조회하는 쿼리 메소드 추가
    Related to: #44

--- src/main/java/com/prgrms2/java/bitta/feed/repository/FeedRepository.java
+++ src/main/java/com/prgrms2/java/bitta/feed/repository/FeedRepository.java
@@ -1,14 +1,20 @@
 package com.prgrms2.java.bitta.feed.repository;
 
 import com.prgrms2.java.bitta.feed.entity.Feed;
+import com.prgrms2.java.bitta.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    @Query("SELECT f FROM Feed f WHERE f.member = :member")
+    List<Feed> findAllByMember(@Param("member") Member member);
+
     @Modifying
     @Query("DELETE FROM Feed f WHERE f.id = :id")
     Long deleteByIdAndReturnCount(@Param("id") Long id);
