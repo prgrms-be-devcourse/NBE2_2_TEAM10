851f023dacb9ee196c3fbc62a593cc7032461337
juwon-code <juwoncode@gmail.com>
2024-10-02 03:26:35 +0900

    feat: 피드 컨트롤러 수정 - 피드 컨트롤러 API 문서화 - 이미지, 비디오 업로드 메소드 제거 - 삽입 및 수정 메소드 수정
    Related to: #48, #58

--- src/main/java/com/prgrms2/java/bitta/feed/controller/FeedController.java
+++ src/main/java/com/prgrms2/java/bitta/feed/controller/FeedController.java
@@ -2,33 +2,56 @@ package com.prgrms2.java.bitta.feed.controller;
 
 import com.prgrms2.java.bitta.feed.dto.FeedDTO;
 import com.prgrms2.java.bitta.feed.service.FeedService;
-import com.prgrms2.java.bitta.member.service.MemberService;
-import com.prgrms2.java.bitta.photo.service.PhotoService;
-import com.prgrms2.java.bitta.video.service.VideoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import static com.prgrms2.java.bitta.global.constants.ApiResponses.*;
+
+@Tag(name = "피드 API 컨트롤러", description = "피드와 관련된 REST API를 제공하는 컨틀롤러입니다.")
 @RestController
 @RequestMapping("/api/v1/feed")
 @RequiredArgsConstructor
 @Validated
 public class FeedController {
     private final FeedService feedService;
-    private final PhotoService photoService;
-    private final VideoService videoService;
-    private final MemberService memberService;
 
-
-
+    @Operation(
+            summary = "전체 피드 조회",
+            description = "전체 피드를 조회합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "피드를 성공적으로 조회했습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_SUCCESS_READ_ALL)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "피드가 존재하지 않습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_NOT_FOUND)
+                            )
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<?> getFeed() {
         return ResponseEntity.ok(
@@ -36,6 +59,43 @@ public class FeedController {
         );
     }
 
+    @Operation(
+            summary = "단일 피드 조회",
+            description = "하나의 피드를 조회합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "피드를 성공적으로 조회했습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_SUCCESS_READ)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "0 이하의 ID를 입력했습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_WRONG_PATH_VARIABLE)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "피드가 존재하지 않습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_NOT_FOUND)
+                            )
+                    )
+            }
+    )
+    @Parameter(
+            name = "id",
+            description = "조회할 피드의 ID",
+            required = true,
+            example = "1",
+            schema = @Schema(type = "integer", minimum = "1")
+    )
     @GetMapping("/{id}")
     public ResponseEntity<?> getFeedById(@PathVariable("id") @Min(1) Long id) {
         return ResponseEntity.ok(
@@ -43,50 +103,157 @@ public class FeedController {
         );
     }
 
-    @PostMapping
-    public ResponseEntity<?> createFeed(@RequestBody @Valid FeedDTO feedDto) {
-        feedService.insert(feedDto);
+    @Operation(
+            summary = "피드 등록",
+            description = "피드를 등록합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "피드가 등록되었습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_SUCCESS_CREATE)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "ID 필드가 비어있지 않습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_BAD_REQUEST)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "파라미터 검증에 실패했습니다. (ex) 비어있는 제목 필드",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_WRONG_REQUEST_BODY)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "파일을 등록하는데 실패했습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_INTERNAL_ERROR)
+                            )
+                    )
+            }
+    )
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> createFeed(@RequestPart(value = "feed") @Valid FeedDTO feedDto
+            , @RequestPart(value = "files", required = false) List<MultipartFile> files) {
+        feedService.insert(feedDto, files);
 
         return ResponseEntity.ok().body(Map.of("message", "피드가 등록되었습니다."));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> modifyFeed(@PathVariable("id") @Min(1) Long id, @RequestBody @Valid FeedDTO feedDTO) {
+    @Operation(
+            summary = "피드 수정",
+            description = "피드를 수정합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "피드가 수정되었습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_SUCCESS_MODIFY)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "0 이하의 ID를 입력했습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_WRONG_PATH_VARIABLE)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "파라미터 검증에 실패했습니다. (ex) 비어있는 제목 필드",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_WRONG_REQUEST_BODY)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "수정할 피드가 존재하지 않습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_NOT_FOUND)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "파일을 수정하는데 실패했습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_INTERNAL_ERROR)
+                            )
+                    )
+            }
+    )
+    @Parameter(
+            name = "id",
+            description = "수정할 피드의 ID",
+            required = true,
+            example = "1",
+            schema = @Schema(type = "integer", minimum = "1")
+    )
+    @PutMapping(value = "/{id}", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> modifyFeed(@PathVariable("id") @Min(1) Long id, @RequestPart("feed") @Valid FeedDTO feedDTO
+            , @RequestPart("filesToUpload") List<MultipartFile> filesToUpload, @RequestPart("filepathsToDelete") List<String> filepathsToDelete) {
         feedDTO.setId(id);
 
-        feedService.update(feedDTO);
+        feedService.update(feedDTO, filesToUpload, filepathsToDelete);
 
         return ResponseEntity.ok().body(Map.of("message", "피드가 수정되었습니다."));
     }
 
+    @Operation(
+            summary = "피드 삭제",
+            description = "피드를 삭제합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "피드가 삭제되었습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_SUCCESS_DELETE)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "0 이하의 ID를 입력했습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_WRONG_PATH_VARIABLE)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "삭제할 피드가 존재하지 않습니다.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(example = FEED_FAILURE_NOT_FOUND)
+                            )
+                    )
+            }
+    )
+    @Parameter(
+            name = "id",
+            description = "삭제할 피드의 ID",
+            required = true,
+            example = "1",
+            schema = @Schema(type = "integer", minimum = "1")
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteFeed(@PathVariable("id") @Min(1) Long id) {
         feedService.delete(id);
 
         return ResponseEntity.ok().body(Map.of("message", "피드가 삭제되었습니다."));
-    }
-
-
-    @PostMapping("/{id}/photos")
-    public ResponseEntity<?> addPhotosToFeed(@PathVariable("id") Long feedId, @RequestParam("photos") List<MultipartFile> photos) {
-        try {
-            feedService.addPhotosToFeed(feedId, photos);
-            return ResponseEntity.ok().body(Map.of("message", "사진이 업로드되었습니다."));
-        } catch (IOException e) {
-            return ResponseEntity.status(500).body("사진 업로드 실패");
-        }
-
-    }
-
-    @PostMapping("/{id}/videos")
-    public ResponseEntity<?> addVideosToFeed(@PathVariable("id") Long feedId, @RequestParam("videos") List<MultipartFile> videos) {
-        try {
-            feedService.addVideosToFeed(feedId, videos);
-            return ResponseEntity.ok().body(Map.of("message", "비디오가 업로드되었습니다."));
-        } catch (IOException e) {
-            return ResponseEntity.status(500).body("비디오 업로드 실패");
-        }
-
     }
 }
 
