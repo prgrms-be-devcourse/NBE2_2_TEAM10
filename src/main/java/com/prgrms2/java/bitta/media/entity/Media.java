22af62f9f2e3b168ae7acc1385b151103305547c
juwon-code <juwoncode@gmail.com>
2024-10-07 00:05:11 +0900

    feat: 미디어 통합 컴포넌트 추가 사진과 비디오를 통합한 컴포넌트를 추가합니다. - 파일정보를 담을 미디어 엔티티 및 DTO 추가 - 미디어 파일 오류 예외 처리 - DB와 통신할 미디어 레포지토리 추가 - 피드 서비스와 연동되는 미디어 서비스 추가
    Related to: #75

---
+++ src/main/java/com/prgrms2/java/bitta/media/entity/Media.java
@@ -0,0 +1,50 @@
+package com.prgrms2.java.bitta.media.entity;
+
+import com.prgrms2.java.bitta.feed.entity.Feed;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Media {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(length = 36, unique = true, nullable = false)
+    private String filename;
+
+    @Column(nullable = false)
+    private String extension;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Long size;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MediaCategory type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
